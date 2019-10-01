bb008d36cfac6cf206140ec65a30246f3d9c1ccb
jerry1177 <jerrymaldonadom@gmail.com>
2019-10-01 10:45:31 -0700

    implemented the firebase dependancies to project. Can now use firebase.

--- app/src/main/java/com/example/student/MainActivity.java
+++ app/src/main/java/com/example/student/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,9 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView myView = findViewById(R.id.textView);
+        String student = BuildConfig.ApiKey;
+        myView.setText(student);
     }
 
     public void jerr(View view){
-        
+        String student = BuildConfig.ApiKey;
     }
 }
